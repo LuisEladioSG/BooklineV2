ae288581b548fecdbb7283e440202d14507fe210
Luis Eladio <102350109+LuisEladioSG@users.noreply.github.com>
2023-05-24 23:55:35 +0200

    Add files via upload

---
+++ bookapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Category, Book, BookSearch
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookSearch)
